2f140f5203ad23b5b7651c9b98418e16041f0b47
roodey21 <fahrudin.abadi@students.amikom.ac.id>
2020-08-06 09:24:24 +0700

    update script

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SportService } from '../services/sport.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  public sport = {};
+  public next = {};
+  public last = {};
 
+  constructor(private sportService: SportService) {}
+
+  ngOnInit(): void {
+    this.sportService.getData().subscribe(result  => {
+      this.sport = result;
+    });
+    this.sportService.getNext().subscribe(result  => {
+      this.next = result;
+    });
+    this.sportService.getPast().subscribe(result  => {
+      this.last = result;
+    });
+  }
 }
